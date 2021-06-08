f9014db2d1d03c8a917f3767b62257629f6c23d2
Daniel Yu <daniel.yu@leaselock.com>
2021-06-08 11:26:33 -0700

    Add GET /buckets endpoint to list S3 buckets.

--- src/app.py
+++ src/app.py
@@ -1,13 +1,24 @@
 from flask import Flask
 
+import boto3
 import os
 
 app = Flask(__name__)
+
+if os.environ.get('AWS_ENDPOINT_URL') is None:
+  session = boto3.Session(profile_name = 'dyu-tmp')
+  s3 = session.client('s3')
+else:
+  s3 = boto3.client('s3', endpoint_url = os.environ.get('AWS_ENDPOINT_URL'), use_ssl = False)
 
 @app.route('/')
 def index():
   print('ping!')
   return 'Web App with Python Flask!'
+
+@app.route('/buckets')
+def get_buckets():
+  return ''.join(map(lambda bucket: bucket['Name'] + '\n', s3.list_buckets()['Buckets']))
 
 app.run(host='0.0.0.0', port=os.environ.get('PORT'))
 
